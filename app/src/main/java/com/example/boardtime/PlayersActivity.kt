99fc44cf896ac1a46362938455aa3cf95cc70fb1
Dawson Neves de Araujo <dawsonaraujo13@gmail.com>
2019-12-20 20:11:29 -0500

    Created players activity

--- app/src/main/java/com/example/boardtime/PlayersActivity.kt
+++ app/src/main/java/com/example/boardtime/PlayersActivity.kt
@@ -3,10 +3,10 @@ package com.example.boardtime
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class PlayerActivity : AppCompatActivity() {
+class PlayersActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_player)
+        setContentView(R.layout.activity_players)
     }
 }
